d314d7a98929de01d35a3dda843087a9079e3911
Semyon Levin <231188+remal@users.noreply.github.com>
2024-09-30 20:02:24 -0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import * as core from '@actions/core'
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+async function run(): Promise<void> {
+    try {
+        core.info('action logic')
+
+    } catch (error) {
+        core.setFailed(error instanceof Error ? error : `${error}`)
+        throw error
+    }
+}
+
+//noinspection JSIgnoredPromiseFromCall
+run()
